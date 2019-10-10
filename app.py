165ce08500235f178eab6a0fdd9e0a2c2a4ff372
HideMe <vahiwe@gmail.com>
2019-10-10 15:49:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import mysql.connector
 from sqlalchemy import create_engine
 import turicreate as tc
 from flask import Flask, jsonify
+
 APP = Flask(__name__)
 
 # The details for connecting to the sql database of lucid blog which was hosted remotely
@@ -14,20 +15,28 @@ MYDB = mysql.connector.connect(host="remotemysql.com", user="8SawWhnha4", passwd
 
 # The mysql engine to be queried and read
 ENGINE = create_engine('mysql+mysqlconnector://8SawWhnha4:zFvOBIqbIz@remotemysql.com/8SawWhnha4')
+
+# Queried Tables 
 USERS = pd.read_sql_query("SELECT * FROM users", ENGINE)
 ARTICLES = pd.read_sql_query("""SELECT id AS post_id,title,content,tags FROM posts""", ENGINE)
 
+# Data cleaning on titles column of articles
 ARTICLES['title'] = ARTICLES['title'].apply(lambda x: x.strip().lower())
 ARTICLES['title'] = ARTICLES['title'].fillna('')
 
 #Construct a reverse map of indices and post titles
 INDEX = pd.Series(ARTICLES['post_id'].values, index=ARTICLES['title'].values)
 INDEX2 = pd.Series(ARTICLES['title'].values, index=ARTICLES['post_id'].values)
+
+# Create Dataframes
 ALLUSERS = pd.DataFrame(USERS)
 MAN = tc.SFrame(data=USERS)
 
+# Load models
 USER_MODEL = tc.load_model("user_model")
 ARTICLE_MODEL = tc.load_model("article_model")
+
+# Homepage html body
 REPORT = "<h2>How to use the API:</h2><p>This is a recommendation system for users and articles. </p><h3>Using Flask</h3><p>To get a user recommendation use this url </p><code>http://127.0.0.1:5000/user/username</code><p> For example:</p><code>http://127.0.0.1:5000/user/eniayomi</code><br><br><p>To get an article recommendation use this url</p><code>http://127.0.0.1:5000/article/title</code><p> For example:</p><code>http://127.0.0.1:5000/article/html begins here</code><br><br><h3>Using Docker</h3><p>To get a user recommendation use this url </p><code>http://0.0.0.0:5000/user/username</code><p> For example:</p><code>http://0.0.0.0:5000/user/eniayomi</code><br><br><p>To get an article recommendation use this url</p><code>http://0.0.0.0:5000/article/title</code><p> For example:</p><code>http://0.0.0.0:5000/article/html begins here</code>" # pylint: disable=line-too-long
 
 @APP.route('/', methods=['GET'])
@@ -48,10 +57,7 @@ def prediction(username):
         _a = ALLUSERS[ALLUSERS['username'] == username]
         _b = _a['id'].values[0]
         _c = _a['name'].values[0]
-        # print('Hello,',c,', here are some users for you to follow...')
         return jsonify(names=list(USER_MODEL.recommend(users=[_b], k=3)['name']))
-        # print("Looks like you are a new user\n Here are some people to follow")
-        # print(model.recommend(users=[4924], k=3))
     return jsonify(names=list(USER_MODEL.recommend(users=[4924], k=3)['name']))
 
 @APP.route('/article/<string:title>', methods=['GET'])
@@ -71,7 +77,6 @@ def article(title):
         post_id = [rec[x]['post_id'] for x, i in enumerate(rec)]
 
         # Return the top 5 most similar posts
-        print("RECOMMENDATIONS BASED ON YOUR READING PATTERN\n")
         return jsonify(articles=list(INDEX2.loc[post_id]))
 
     except KeyError:
